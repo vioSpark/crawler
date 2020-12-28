65565dd89c151309dcfbffdb1314b35cce53ed71
lukacs.mark@gmail.com <vioSpark@users.noreply.github.com>
2020-12-28 19:49:43 +0100

    gone to dinner

--- src/crawler.py
+++ src/crawler.py
@@ -5,64 +5,89 @@ import os
 import re
 import pickle
 import datetime as dt
-
+import networkx as nx
+import matplotlib.pyplot as plt
+import logging
+import sys
 
 # import networkx as nx
 
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(sys.stdout)
+log.addHandler(handler)
+
 
 class Crawler:
+    graph = nx.DiGraph()
     base_url = None
     recursion_level = None
-    visited = []
+    # visited = []
     browser = mechanicalsoup.StatefulBrowser()
+    quantity_limit = 5
 
     def config(self, base_url, recursion_level):
         self.base_url = base_url
         self.recursion_level = recursion_level
 
-    def crawl(self, link):
+    def crawl(self, url, origin):
+        def set_log_indent(self):
+            tmp = ''
+            for i in range(self.recursion_level):
+                tmp += '\t'
+            return tmp
+
+        def error_check(self, new_url, number_of_visited_links, tmp):
+            if number_of_visited_links > self.quantity_limit:
+                log.debug(tmp + 'quantity limit reached, returning')
+                return True
+            if new_url.startswith('#'):
+                log.debug(tmp + 'reference to the same page, omitting:\t' + new_url)
+                return True
+            if new_url[:2] == '//':
+                log.debug(tmp + 'outside of the boundaries, omitting:\thttps:' + new_url)
+                return True
+            return False
+
+        number_of_visited_links = 0
+        self.graph.add_edge(origin, url)
+        self.download_page(url)
+
+        tmp = set_log_indent(self)
+
+        if self.recursion_level > 1:
+            log.debug(tmp + 'depth limit reached, returning')
+            # self.recursion_level -= 1
+            return
+
         self.recursion_level += 1
-        print('crawling started at recursion level:\t' + str(self.recursion_level))
-        for visit in self.visited:
-            if visit == link:
-                print('already visited:\t\t\t\t' + str(link))
-                return
-        print('not visited yet:\t\t\t\t' + str(link))
-        self.browser.open(link)
-        print('link opened')
-        self.visited.append(link)
-        print('link logged')
+
+        log.debug(tmp + 'crawling started at recursion level:\t' + str(self.recursion_level))
+        self.browser.open(url)
+        log.debug(tmp + 'link opened:\t' + url)
+
         for link in self.browser.links():
-            print()
-            url = link.attrs['href']
-            # link to self
-            if url.startswith('#'):
-                print('reference to the same page:\t' + url)
+            number_of_visited_links += 1
+            new_url = link.attrs['href']
+
+            error = error_check(self, new_url, number_of_visited_links, tmp)
+            if error:
                 continue
-            if url[:2] == '//':
-                print('outside of the boundaries:\thttps:' + url)
-                continue
-            if url.startswith(self.base_url) or url.startswith('/'):
-                if url.startswith('/'):
-                    # self.base_url.split('/')
-                    # url = self.base_url + url
-                    # searching for the 3rd '/' with regex,
-                    # trimming the last character down, than appending the old url
-                    url = re.findall('(^(?:.*?\/){3})', self.base_url)[0][:-1] + url
+            if new_url.startswith(self.base_url) or new_url.startswith('/'):
+                if new_url.startswith('/'):
+                    # trimming the last character down, than appending the new_url
+                    new_url = re.findall('(^(?:.*?\/){3})', self.base_url)[0][:-1] + new_url
+                    if new_url in self.graph.nodes:
+                        log.debug(tmp + 'already visited, omitting:\t' + new_url)
+                        continue
                 try:
-                    self.download_page(url)
-                except HTTPError as e:
-                    print("\tERROR:\t\t\t\t\t" + str(e))
-                    continue
-                except UnicodeDecodeError as e:
-                    print("\tERROR:\t\t\t\t\t" + str(e))
-                    continue
-                except OSError as e:
-                    print("\tERROR:\t\t\t\t\t" + str(e))
+                    self.crawl(new_url, self.browser.get_url())
+                except HTTPError or UnicodeDecodeError or OSError as e:
+                    print(tmp + "ERROR:\t\t\t\t\t" + str(e))
             else:
-                print('outside of the boundaries:\t' + url)
+                log.debug(tmp + 'outside of the boundaries, omitting:\t' + new_url)
                 continue
-        print('crawling stopped on recursion level:\t' + str(self.recursion_level))
+        log.debug(tmp + 'crawling stopped on recursion level:\t' + str(self.recursion_level))
         self.recursion_level -= 1
 
     def run(self):
@@ -72,24 +97,32 @@ class Crawler:
             pass
 
         try:
-            self.crawl(self.base_url)
+            self.crawl(self.base_url, 'START')
         except ... as e:
             print("general error occurred: " + str(e))
-        print(self.visited)
-        file = open("../data/visited", 'ab')
-        pickle.dump(self.visited, file)
-        file.close()
+        print(list(self.graph.nodes()))
+        self.save()
 
-    def download_page(self, url):
-        print("getting:\t\t\t\t" + url)
-        page = urlopen(url)
+    def save(self):
+        pass
+        # file = open("../data/visited", 'ab')
+        # pickle.dump(list(self.graph.nodes()), file)
+        # file.close()
+
+    @staticmethod
+    def download_page(url):
+        pass
+        # log.info("downloading:\t" + url)
+        # page = urlopen(url)
         # print("data/"+re.sub('[\r\n?]*','',str(link.text)) +".htm")
-        print('\tomitting download, only testing')
-        self.crawl(url)
+        # log.debug('\tomitting download, only testing')
         # file=open("data/"+re.sub('[\r\n?*]','',str(link.text)) +".htm", "w")
         # file.write(page)
         # file.close()
 
     def visualize(self):
-        pass
-        # g = nx.Graph()
+        plt.figure(111, figsize=(12, 12))
+        pos = nx.spring_layout(self.graph, k=30, iterations=1000)
+        nx.draw(self.graph, pos=pos, with_labels=True, node_size=60, font_size=20)
+        # nx.draw(self.graph, with_labels=True, font_weight='bold')
+        plt.show()
--- src/main.py
+++ src/main.py
@@ -1,10 +1,6 @@
 import crawler
-# import nx_tutorial
-# import matplotlib_test
 
-# matplotlib_test.test()
-# nx_tutorial.test()
-
+print()
 cr = crawler.Crawler()
 cr.config(base_url="https://en.wikipedia.org/wiki/Graph_(discrete_mathematics)", recursion_level=0)
 cr.run()
